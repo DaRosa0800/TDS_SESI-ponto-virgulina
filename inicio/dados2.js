03fe4e00ee1408a4bbda92719fd60f9b76a6be6e
DaRosa0800 <borel0060@gmail.com>
2026-02-27 11:47:07 -0300

    Tipos de dados em JS

---
+++ inicio/dados2.js
@@ -0,0 +1,10 @@
+console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+//console.log(precoComDesconto);
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+console.log("Produto: " + nome + " | Categoria: " + categoria + " | Preço: " + preco + " | Desconto: " + (desconto*100) + "% | Preço Final: " + precoComDesconto);
--- inicio/desafio_bloco.js
+++ inicio/desafio_bloco.js
@@ -19,10 +19,10 @@
 
 {
     console.log("--Ímpares--");
-    console.log("     1");
-    console.log("     3");
-    console.log("     5");
-    console.log("     7");
-    console.log("     9");
+    console.log("    1");
+    console.log("    3");
+    console.log("    5");
+    console.log("    7");
+    console.log("    9");
     console.log("    ");
 }
