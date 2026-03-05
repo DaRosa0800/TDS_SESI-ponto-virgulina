31e90249e901a4d9bbc0bb231a27fab4f52fc8b7
Emanuel Lampadino <165965677+DaRosa0800@users.noreply.github.com>
2026-03-05 12:16:41 -0300

    Add files via upload

---
+++ aritmeticos.js
@@ -0,0 +1,27 @@
+// valores literais
+console.log("------VALORES LITERÁIS------")
+console.log("Adição: 10+5");
+console.log(10 + 5); // adição
+
+console.log("Subtração: 10-5");
+console.log(10 - 5);
+
+console.log("Multiplicação: 10*5");
+console.log(10 * 5);
+
+console.log("Divisão: 10/5");
+console.log(10 / 5);
+
+console.log("Resto da divisão: 10%5");
+console.log(10 % 5);
+
+// valores variaveis
+
+let a = 100;
+let b = 50;
+console.log("------VARIÁVEIS-----")
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
