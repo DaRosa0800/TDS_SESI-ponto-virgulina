403f735b37880a344809d195077795b433a38940
DaRosa0800 <borel0060@gmail.com>
2026-03-06 11:40:20 -0300

    operações aritméticas e ralacionados

---
+++ operadores/desafio_relacionados.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 11;
+
+// Imprimir no console 5 números verdadeiros (true)
+
+console.log(b > a);
+console.log(a > b);
+console.log(a < b);
+console.log(b >= a);
+console.log(a <= b);
+console.log(a == a);
+
+console.log("\n");
+
+// Imprimir no console 5 númeos falsos (false)
+
+console.log(a < b);
+console.log(b < a);
+console.log(a > b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a != a);
