eb176fc17c6e112b0fa7ffe7c13ef48d8e9bf693
DaRosa0800 <borel0060@gmail.com>
2026-03-13 12:20:19 -0300

    arraysing js

---
+++ array/array2.js
@@ -0,0 +1,9 @@
+// Desafio: inserir 5 novos indices com 5 nomes e imprimir na tela
+const nome = [];
+nome[0] = "Pedro";
+nome[1] = "Nathalia";
+nome[2] = "Miguel";
+nome[3] = "Harry Potter";
+nome[3] = "Pedro po";
+console.log(nome);
+console.log("\n" + nome.length);
