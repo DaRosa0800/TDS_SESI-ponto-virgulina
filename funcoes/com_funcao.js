5175601ef3be964765a90b36cc145913f11cc379
DaRosa0800 <borel0060@gmail.com>
2026-03-19 12:16:53 -0300

    feita ai as coisa da aula

---
+++ funcoes/com_funcao.js
@@ -0,0 +1,18 @@
+const produto1 = "Notebook";
+const preco1 = 3500;
+
+function exibirProduto(produto, preco){
+    console.log(`${produto1} -> ${preco1}`);
+}
+
+const produto2 = "Urso";
+const preco2 = 1309047;
+
+// Desafio: Criar segunda function para produto2
+
+function exibirProduto2(produto, preco) {
+    console.log(`${produto2} -> ${produto2}`);
+}
+
+exibirProduto();
+exibirProduto2();
---
+++ funcoes/com_funcao2.js
@@ -0,0 +1,13 @@
+const produto1 = "notebook";
+const preco1 = 3500;
+
+const produto2 = "urso";
+const preco2 = 487346801;
+
+function exibirProduto(produto, preco) {
+    console.log(`${produto} - ${preco}`);
+}
+
+// Como imprimir os dois produtos com os seus preços
+exibirProduto(produto1, preco1);
+exibirProduto(produto2, preco2);
