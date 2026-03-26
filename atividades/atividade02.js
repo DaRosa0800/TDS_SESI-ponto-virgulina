8b0069310dd4641d7702b8afc6291ed9a5d4d28c
DaRosa0800 <borel0060@gmail.com>
2026-03-26 11:57:29 -0300

    Atividade 2

---
+++ atividades/atividade02.js
@@ -0,0 +1,8 @@
+let idade_cachorro = 0;
+let idade_cachorro2 = idade_cachorro * 7;
+
+if(idade_cachorro >= 1){
+    console.log("A idade do cachorro calculada para a idade dos humanos é: " + idade_cachorro2);
+}else {
+    console.log("Insira um número válido maior que 0");
+}
