2b80e13d33fe822a496e2e00d9924a73fba16eae
DaRosa0800 <borel0060@gmail.com>
2026-03-27 12:10:39 -0300

    atividade 4 a 7 feita

---
+++ atividades/atividade04.js
@@ -0,0 +1,4 @@
+const compra = 10;
+let descontoDaCompra = compra >= 100 ? descontoDaCompra = true && compra - (compra * 0.10) : descontoDaCompra = false && compra;
+
+console.log(`O  preç final da compra foi ${descontoDaCompra}`);
