dbd743f64cea734d82eeab641b672307361d5fc1
DaRosa0800 <borel0060@gmail.com>
2026-04-02 12:12:46 -0300

    feita atividades 8 até 11

---
+++ atividades/atividade10.js
@@ -0,0 +1,6 @@
+let hardwaresLista = ["CPU", "GPU", "Fonte", "Placa-mãe", "SSD", "RAM"];
+
+hardwaresLista.push ("CPU");
+hardwaresLista.shift ();
+
+console.log(hardwaresLista);
---
+++ atividades/atividade11.js
@@ -0,0 +1,8 @@
+let alunosLista = ["Lobisome do Arvoredo", "Todo", "Mundo", "Tinha", "Medo", "lorin do Fut", "João Bi", "Jeremias", "Lobisominho", "Lobisoma"];
+console.log(alunosLista);
+
+if (alunosLista.includes("Lobisome do Arvoredo")) {
+    console.log("O nome do Lobisome do Arvoredo ta na lista!");
+} else {
+    console.log("O nome Lobisome do Arvoredo não ta na lista!");
+}
